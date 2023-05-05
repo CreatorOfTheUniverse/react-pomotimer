d71967790f07a0cc6fa47671e4e97bbeba2fff18
CreatorOfTheUniverse <abakumov.dmitriy2012@gmail.com>
2023-05-06 04:17:36 +0500

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 type ChildrenProps = {
@@ -10,7 +10,12 @@ function Container({children}: ChildrenProps) {
 }
 
 function Header({children}: ChildrenProps) {
-    return <header className="flex justify-between py-2 border-b border-red-700">{children}</header>;
+    return (
+        <>
+            <header className="flex justify-between py-2 border-red-700">{children}</header>
+            <div className="border-b-[3px] rounded w-[21%]"></div>
+        </>
+    )
 }
 
 function HeaderLogo() {
@@ -47,16 +52,72 @@ function HeaderButton({text, imgSrc}: HeaderButtonProps) {
 
 function Main({children}: ChildrenProps) {
     return (
-        <div>
+        <div className="p-3">
             {children}
         </div>
     )
 }
 
-function MainTimerWrap({children}: ChildrenProps) {
+function MainWrap({children}: ChildrenProps) {
     return (
-        <div className="mx-20 mt-6 mb-3 py-3 px-20 bg-red-300 rounded-md">
+        <div className="mt-6 mb-3 py-3 bg-red-300 rounded-md max-w-md mx-auto">
             {children}
+        </div>
+    )
+}
+
+function MainButtons() {
+    const [active, setActive] = useState(1)
+    const activeButtonClasses = " bg-red-400 font-bold"
+    const buttonClasses = "flex items-center px-3 rounded opacity-90 hover:opacity-100 transition-colors"
+    const handleClick = (buttonNumber: number) => {
+        return setActive(buttonNumber)
+    }
+    return (
+        <div className="flex justify-center gap-2">
+            <button onClick={() => {
+                handleClick(1)
+            }} className={active === 1 ? buttonClasses + activeButtonClasses : ""} type="button">
+                <p className="whitespace-nowrap">Pomodoro</p>
+            </button>
+            <button onClick={() => {
+                handleClick(2)
+            }} className={active === 2 ? buttonClasses + activeButtonClasses : ""} type="button">
+                <p className="whitespace-nowrap">Short Break</p>
+            </button>
+            <button onClick={() => {
+                handleClick(3)
+            }} className={active === 3 ? buttonClasses + activeButtonClasses : ""} type="button">
+                <p className="whitespace-nowrap">Long Break</p>
+            </button>
+        </div>
+    )
+}
+
+function MainTimer() {
+    const [time, setTime] = useState(10)
+    const [startTimer, setStartTimer] = useState(false)
+    const [switchButton, setSwitchButton] = useState(true)
+    const textButton = switchButton ? "START" : "PAUSE"
+    useEffect(() => {
+        if (startTimer) {
+            if (time !== 0) {
+                setTimeout(() => {
+                    setTime(prevState => time - 1)
+                }, 1000)
+            }
+        }
+    }, [time, startTimer])
+    return (
+        <div className="flex flex-col items-center">
+            <div className="text-8xl font-bold text-center my-6">
+                {time}
+            </div>
+            <button onClick={() => {
+                setStartTimer(true)
+                setSwitchButton(!switchButton)
+            }} type="button" className="px-7 py-3 mb-3 w-48 mx-auto bg-white rounded drop-shadow-lg text-2xl text-red-400 font-bold">{textButton}
+            </button>
         </div>
     )
 }
@@ -73,9 +134,10 @@ function App() {
                 </HeaderButtonsWrap>
             </Header>
             <Main>
-                <MainTimerWrap>
-                    fghg
-                </MainTimerWrap>
+                <MainWrap>
+                    <MainButtons/>
+                    <MainTimer/>
+                </MainWrap>
             </Main>
         </Container>
     );
